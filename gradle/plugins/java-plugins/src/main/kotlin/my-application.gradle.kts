918315731860a5d31d5609e987e87ad146651e81
PeronVolodymyr <perwolod@gmail.com>
2022-12-03 20:50:20 +0100

    fix

--- gradle/plugins/java-plugins/src/main/kotlin/my-application.gradle.kts
+++ gradle/plugins/java-plugins/src/main/kotlin/my-application.gradle.kts
@@ -1,5 +1,6 @@
 // Custom convention plugin - mainly composition of another plugins.
-// Plugins that only reconfigure existing plugins are typically called 'convention plugin'.plugins {
+// Plugins that only reconfigure existing plugins are typically called 'convention plugin'.
+plugins {
     id("application")
     id("my-java-base")
 }
