bc698f12b0d30fd3e317c0bb5b93d3c5202caea1
PeronVolodymyr <perwolod@gmail.com>
2022-12-04 17:45:00 +0100

    configure tasks with type of JavaCompile

--- gradle/plugins/java-plugins/src/main/kotlin/my-java-base.gradle.kts
+++ gradle/plugins/java-plugins/src/main/kotlin/my-java-base.gradle.kts
@@ -8,3 +8,16 @@ plugins {
 java {
     toolchain.languageVersion.set(JavaLanguageVersion.of(17))
 }
+
+// Be careful as it will configure all tasks with type 'JavaCompile'.
+tasks.withType<JavaCompile>().configureEach {
+    options.encoding = "UTF-8"
+}
+// Comment out the code below as encoding is set in 'tasks.withType<JavaCompile>'
+// for both 'tasks.compileJava' and 'tasks.compileTestJava'.
+//tasks.compileJava {
+//    options.encoding = "UTF-8"
+//}
+//tasks.compileTestJava {
+//    options.encoding = "UTF-8"
+//}
